2f4d755779b9e89280c705151e7e569fe9884033
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-04-28 20:48:11 +0200

    exported controller

--- hermes-server/src/main/java/module-info.java
+++ hermes-server/src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module it.turin.hermesserver {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens it.turin.hermesserver to javafx.fxml;
+    opens it.turin.hermesserver.controller to javafx.fxml;
     exports it.turin.hermesserver;
 }
