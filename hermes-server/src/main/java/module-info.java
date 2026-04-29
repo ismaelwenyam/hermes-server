cf3d9eaf5a4a2ecc3179661f499f1c38f8c8d645
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-04-29 21:31:50 +0200

    opened model and dto package to gson

--- hermes-server/src/main/java/module-info.java
+++ hermes-server/src/main/java/module-info.java
@@ -2,9 +2,12 @@ module it.turin.hermesserver {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires com.google.gson;
 
 
     opens it.turin.hermesserver to javafx.fxml;
     opens it.turin.hermesserver.controller to javafx.fxml;
+    opens it.turin.hermesserver.dto to com.google.gson;
+    opens it.turin.hermesserver.model to com.google.gson;
     exports it.turin.hermesserver;
 }
