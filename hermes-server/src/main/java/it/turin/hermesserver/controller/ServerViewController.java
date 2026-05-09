03ad86ac134b6a19d8b4fd9c9332710b7bf0c33f
ismael_wemyam_arnaud <wenyam1993@gmail.com>
2026-05-09 14:16:33 +0200

    implmented server view controller

---
+++ hermes-server/src/main/java/it/turin/hermesserver/controller/ServerViewController.java
@@ -0,0 +1,43 @@
+package it.turin.hermesserver.controller;
+
+import it.turin.hermesserver.service.MailboxService;
+import it.turin.hermesserver.model.ServerModel;
+import it.turin.hermesserver.network.HermesServerSocket;
+import it.turin.hermesserver.service.RequestService;
+import javafx.application.Platform;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import java.util.Arrays;
+
+public class ServerViewController {
+
+    private HermesServerSocket serverSocket;
+
+    @FXML
+    private ListView<String> logListView;
+
+    public void init (ServerModel serverModel, int port) {
+        ObservableList<String> ol = serverModel.getLogs();
+        logListView.setItems(ol);
+        ol.addListener((ListChangeListener<String>) change -> {
+            while (change.next()) {
+                if (change.wasAdded()) {
+                    Platform.runLater(() ->
+                            logListView.scrollTo(ol.size() - 1)
+                    );
+                }
+            }
+        });
+        MailboxService mailboxManager = new MailboxService(serverModel);
+        RequestService service = new RequestService(mailboxManager, serverModel);
+        mailboxManager.initMailBoxes(Arrays.asList("ismael@hermes.it", "eileen@hermes.it", "francesco@hermes.it"));
+        this.serverSocket =  new HermesServerSocket(port, serverModel, service);
+        serverSocket.init();
+    }
+
+    public void shutdown(){
+        serverSocket.stop();
+    }
+}
